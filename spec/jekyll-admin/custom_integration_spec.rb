140a354406b8e15277cf0d5a6467abf37772dc45
Ashwin Maroli <ashmaroli@gmail.com>
2017-09-24 19:22:49 +0530

    support detecting custom configs passed via CLI

--- spec/jekyll-admin/custom_integration_spec.rb
+++ spec/jekyll-admin/custom_integration_spec.rb
@@ -1,7 +1,18 @@
-describe "integration" do
+describe "custom integration" do
   let(:source) { fixture_path("site") }
   let(:dest) { File.join(source, "_site") }
-  let(:args) { ["--detach", "--watch", "--source", source, "--destination", dest] }
+  let(:config) { File.join(source, "_config_test.yml") }
+
+  let(:args) do
+    [
+      "--detach",
+      "--watch",
+      "--source", source,
+      "--destination", dest,
+      "--config", config,
+    ]
+  end
+
   let(:start_command) { %w(bundle exec jekyll serve).concat(args) }
   let(:stop_command) { ["pkill", "-f", "jekyll"] }
   let(:server) { "http://localhost:4000" }
@@ -37,28 +48,11 @@ describe "integration" do
   end
 
   context "API" do
-    let(:path) { "/_api" }
+    let(:path) { "/_api/configuration" }
 
     it "serves the Jekyll site", :skip => Gem.win_platform? do
       expect(response.code).to eql("200")
-      expect(response.body).to match("collections_api")
-    end
-  end
-
-  context "watching" do
-    it "Jekyll isn't watching", :skip => Gem.win_platform? do
-      File.open(File.join(source, "page.md"), "a") do |f|
-        f.puts "peek-a-boo"
-      end
-      content = File.read(File.join(source, "page.md"))
-      output = File.read(File.join(dest, "page.html"))
-
-      expect(content).to include("peek-a-boo")
-      expect(output).not_to include("peek-a-boo")
-
-      File.open(File.join(source, "page.md"), "w+") do |f|
-        f.write "---\nfoo: bar\n---\n\n# Test Page\n"
-      end
+      expect(response.body).to match("Custom Test Site")
     end
   end
 end
